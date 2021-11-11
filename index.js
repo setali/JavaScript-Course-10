83ce1d1fca886a46367893b51ef5dafcc82f85da
salimousavi <set.ali.mousavi@gmail.com>
2021-11-11 22:59:38 +0330

    Session 2: Data types, arithmetic and comparison operators

---
+++ index.js
@@ -0,0 +1,91 @@
+console.log(('b' + 'a' + +'a' + 'a').toLowerCase()) // banana
+
+// var arr = [12, 'salam', true, 2, 'k', false, { name: 'Ali' }, [2, 3]]
+
+// console.log(arr[arr.length - 1])
+
+// var friends = ['Ali', 'Eli', 'Qoli', 'Fati']
+
+// // console.log(friends.length)
+
+// friends[0] = 'Hasan'
+// console.log(friends)
+
+// var a = 32
+
+// var b = a
+
+// console.log(a, b)
+
+// b = 58
+
+// console.log(a, b)
+
+// #82
+// var person = {
+//   firstName: 'Ali',
+//   lastName: 'Mousavi',
+//   age: 32,
+//   gender: true,
+//   family: {
+//     father: 'Mohammad',
+//     mother: 'Hajar',
+//     wife: 'Eli',
+//     children: ['Mahsa']
+//   },
+//   friends: ['Ali', 'Qoli', 'Eli'],
+//   propertyName: 'TEST'
+// }
+
+// console.log(person)
+
+// person.age = 25
+// person.family.wife = 'Qoli'
+// console.log(person)
+
+// var person2 = person
+
+// console.log(person.firstName)
+// console.log(person2.firstName)
+
+// person2.firstName = 'Hasan'
+
+// console.log(person.firstName)
+// console.log(person2.firstName)
+
+// var settings = {
+//   nickname: 'lastName'
+// }
+
+// console.log('Welcome ' + person[settings.nickname])
+
+// var propertyName = 'age'
+
+// console.log(person[propertyName])
+// console.log(person.propertyName)
+
+// console.log(person.family.father)
+
+// var firstName = 'Ali'
+// var lastName = 'Mousavi'
+
+// var str = 'salam \t khoobi? \n welcome \u03A9'
+// console.log(str)
+
+// var name = 'ali'
+// var age = 32
+
+// var نام = 'علی'
+
+// var firstName = 'Ali',
+//   lastName = 'Mousavi'
+
+// console.log(firstName, lastName)
+// var name2
+
+// 2 + 2
+
+/*
+2 + 6 
+8 * 9
+*/
